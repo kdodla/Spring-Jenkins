5ff0ad735e92ceaa1bc60a5217524450389e3aea
Kalpana <kdodla1983@gmail.com>
2020-08-07 17:29:52 -0400

    first commit

---
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.javatechie.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	Logger logger=LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("I am in testing contextLoads");
+		assertEquals(true,true);
+	}
+
+}
